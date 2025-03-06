6db09ff3aebbaf42e9e086e9c569aed0a2a5e128
Pitchamint <>
2025-03-06 17:31:43 +0700

    update style navbar

--- client/src/layout/Navbar/Navbar.tsx
+++ client/src/layout/Navbar/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex justify-between items-center shadow p-4 w-full">
+    <div className="flex justify-between items-center shadow p-4 w-full gap-10">
       <div className="flex items-center gap-2">
         <AiOutlineMenu />
         <p className="text-lg">📉</p>
@@ -21,14 +21,17 @@ const Navbar = () => {
         <input
           type="text"
           placeholder="ค้นหาข้อมูลโปรโมชั่น ข่าวสารและแคมเปญ"
-          className="w-full px-4 py-2 border border-gray-300 rounded-full bg-white"
+          className="px-4 py-2 border border-gray-300 rounded-full bg-white w-lg"
         />
       </div>
-      <div className="flex items-center h-1/2 gap-2">
-        <AiOutlineBell />
-        <button className="flex items-center gap-2 px-4 py-2"> 🇹🇭 ไทย <AiOutlineDown /> </button>
-      </div>
       <div className="flex items-center gap-3">
+        <div className="flex items-center h-1/2 gap-2">
+          <AiOutlineBell />
+          <button className="flex items-center gap-2 px-4 py-2">
+            {" "}
+            🇹🇭 ไทย <AiOutlineDown />{" "}
+          </button>
+        </div>
         <img
           className="w-10 h-10 rounded-full"
           src={tellerData.image}
@@ -37,11 +40,10 @@ const Navbar = () => {
         <div className="text-sm">
           <p className="font-semibold text-black">{tellerData.name}</p>
           <div className="flex gap-2">
-          <p>{tellerData.id}</p>
-          <p>{tellerData.position}</p>
-          <p>{tellerData.branch}</p>
+            <p>{tellerData.id}</p>
+            <p>{tellerData.position}</p>
+            <p>{tellerData.branch}</p>
           </div>
-          
         </div>
         <AiOutlineDown />
       </div>
