2deb76e0f6573b8b6679ae5f4ae5d84adaa317ca
miles <milse@odds.team>
2025-03-07 15:25:54 +0700

    refactor(test): add before each to setup data in unit test

--- client/src/page/TransactionOverview/TransactionOverview.spec.tsx
+++ client/src/page/TransactionOverview/TransactionOverview.spec.tsx
@@ -1,25 +1,32 @@
 import { render, screen, cleanup } from "@testing-library/react";
-import { test, describe, expect, afterEach } from "vitest";
+import { test, describe, expect, afterEach, beforeEach } from "vitest";
 import TransactionOverview from "./TransactionOverview";
 
 describe("Transaction overview component", () => {
+  let initialData = {
+    mywork: [
+      { name: "A", quantity: 1, amount: 10000 },
+      { name: "B", quantity: 3, amount: 4200 },
+    ],
+    branch: [
+      { name: "A", quantity: 1, amount: 10000 },
+      { name: "B", quantity: 3, amount: 4200 },
+      { name: "C", quantity: 2, amount: 8000 },
+    ],
+    drawer: { remain: 1, total: 3 },
+  };
+
+  let data = initialData;
+
+  beforeEach(() => {
+    data = JSON.parse(JSON.stringify(initialData));
+  });
+
   afterEach(() => {
     cleanup();
   });
 
   test("should calculate all transaction amount correctly", () => {
-    const data = {
-      mywork: [
-        { name: "A", quantity: 1, amount: 10000 },
-        { name: "B", quantity: 3, amount: 4200 },
-      ],
-      branch: [
-        { name: "A", quantity: 1, amount: 10000 },
-        { name: "B", quantity: 3, amount: 4200 },
-        { name: "C", quantity: 2, amount: 8000 },
-      ],
-      drawer: { remain: 1, total: 3 },
-    };
     render(
       <TransactionOverview
         transactionWork={data.mywork}
@@ -34,18 +41,6 @@ describe("Transaction overview component", () => {
   });
 
   test("should calculate all transaction quantity correctly", () => {
-    const data = {
-      mywork: [
-        { name: "A", quantity: 1, amount: 10000 },
-        { name: "B", quantity: 3, amount: 4200 },
-      ],
-      branch: [
-        { name: "A", quantity: 1, amount: 10000 },
-        { name: "B", quantity: 3, amount: 4200 },
-        { name: "C", quantity: 2, amount: 8000 },
-      ],
-      drawer: { remain: 1, total: 3 },
-    };
     render(
       <TransactionOverview
         transactionWork={data.mywork}
@@ -56,4 +51,6 @@ describe("Transaction overview component", () => {
     expect(screen.getByTestId("MyWork-quantity-2").textContent).toEqual("4");
     expect(screen.getByTestId("MyBranch-quantity-3").textContent).toEqual("6");
   });
+
+  test("when drawer data change, it progress bar should also change", () => {});
 });
