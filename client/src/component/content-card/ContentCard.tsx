56995f4af5ead7fcda6f9528fa2a987a159659e3
Donatsuu <donatsu@odds.team>
2025-03-07 13:09:06 +0700

    [SP0 MAIN-PAGE UPDATE] refactor style and layout

--- client/src/component/content-card/ContentCard.tsx
+++ client/src/component/content-card/ContentCard.tsx
@@ -1,7 +1,7 @@
 import { CardRow } from "./interfaces/content-card.interface";
 
 export default function ContentCard({ row, contentList }: CardRow) {
-  const cardStyle = `flex flex-col bg-gray-200 pt-1 m-2 mt-1 border-1 border-solid border-red-500 max-w-[190%]`;
+  const cardStyle = `flex flex-col pt-1 m-2 mt-1 max-w-[190%]`;
   const rowItems = new Array(row)
     .fill(Object.keys(Array(row)), 0, row)
     .map((_row, index) => (
--- client/src/page/MainPage/MainPage.tsx
+++ client/src/page/MainPage/MainPage.tsx
@@ -3,29 +3,22 @@ import TransactionMenu from "../../components/TransactionMenu/TransactionMenu";
 import TransactionOverview from "../TransactionOverview/TransactionOverview";
 export default function MainPage() {
   const contentCardBorder =
-    "p-2 bg-[#F3F7F8] rounded-sm border-2 border-red-500";
+    "p-2";
   return (
-    <div>
-      <div className={`h-[2em] ${contentCardBorder}`}>nav-bar</div>
-      <div className={`h-[2.5em] ${contentCardBorder}`}>
-        สวัสดีครับคุณธนชาติ
+    <div className="mt-20 mb-20 min-h-[100vh] grid grid-cols-[2fr_3fr_2fr] bg-[#F3F7F8]">
+      <div className={contentCardBorder}>
+        <TransactionOverview />
       </div>
-      <div className="grid grid-cols-[2fr_3fr_2fr] bg-[#F3F7F8] h-[100vh]">
-        <div className={contentCardBorder}>
-          <TransactionOverview />
-        </div>
-        <ContentCard
-          row={1}
-          contentList={[
-            {
-              title: "เมนูธุรกรรม",
-              content: <TransactionMenu></TransactionMenu>,
-            },
-          ]}
-        ></ContentCard>
-        <div className={contentCardBorder}>รายการอื่นๆ</div>
-      </div>
-      <div className={contentCardBorder}>Footer</div>
+      <ContentCard
+        row={1}
+        contentList={[
+          {
+            title: "เมนูธุรกรรม",
+            content: <TransactionMenu></TransactionMenu>,
+          },
+        ]}
+      ></ContentCard>
+      <div className={contentCardBorder}>รายการอื่นๆ</div>
     </div>
   );
 }
