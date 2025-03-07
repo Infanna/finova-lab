7f6562618af0962ccd93d4d4f310eb60a4c38c16
Donatsuu <donatsu@odds.team>
2025-03-07 17:31:45 +0700

    [SP0 MAIN PAGE UPDATE] add rating to screen

--- client/src/page/MainPage/MainPage.tsx
+++ client/src/page/MainPage/MainPage.tsx
@@ -43,7 +43,7 @@ export default function MainPage() {
         contentList={[
           {
             title: "เมนูธุรกรรม",
-            content: <TransactionMenu></TransactionMenu>,
+            content: <Rating></Rating>,
           },
         ]}
       ></ContentCard>
