53bfb314ccc355123315d44f7906c2e31d00a105
Pitchamint <>
2025-03-07 11:29:45 +0700

    remove unuse page

--- client/src/router/appRouter.tsx
+++ client/src/router/appRouter.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../page/main/main";
-import Page1 from "../page/page1";
 import Withdraw from "../page/Withdraw/Withdraw";
 import Navbar from "../layout/Navbar/Navbar";
 import Footer from "../layout/Footer/Footer";
@@ -25,10 +24,6 @@ const AppRouter = createBrowserRouter([
         <Footer />
       </>
     ),
-  },
-  {
-    path: "/page1",
-    element: <Page1 />,
   },
 ]);
 
